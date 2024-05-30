3dc55b4f9be0f24da44dfb47bd44072219daf0c7
AlexanderGrebenyuk <suppose.ca@gmail.com>
2024-05-30 15:48:25 +0300

    feat:addAuth

--- server/routes/api/auth.api.routes.js
+++ server/routes/api/auth.api.routes.js
@@ -1,51 +1,50 @@
-const bcrypt = require("bcrypt");
+const bcrypt = require('bcrypt');
 
-// const router = require("express").Router();
-// const { Registration } = require("../../db/models");
-// const generateTokens = require("../../utils/generateTokens");
-// const cookiesConfig = require("../../configs/cookiesConfig");
+const router = require('express').Router();
+const { User } = require('../../db/models');
+const generateTokens = require('../../utils/generateTokens');
+const cookiesConfig = require('../../configs/cookiesConfig');
 
-// router.post("/login", async (req, res) => {
-//     const { email, password } = req.body; /// 111111
-//     try {
-//         const targetUser = await Registration.findOne({ where: { email} });
-//         if (!targetUser) return res.status(401).json({ message: "Неверный email или пароль" });
-        
-//         const isValid = await bcrypt.compare(password, targetUser.password);
-//         if (!isValid) return res.status(401).json({ message: "Неверный email или пароль" })
-        
-//         const user = targetUser.get();
-//         delete user.password
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body; /// 111111
 
-    
-//         const {accessToken, refreshToken} = generateTokens({user})
-        
-//         res
-//         .cookie("refreshToken", refreshToken,cookiesConfig)
-//         .json({accessToken, user});
+  try {
+    const targetUser = await User.findOne({ where: { email } });
+    if (!targetUser)
+      return res.status(401).json({ message: 'Неверный email или пароль' });
+    const isValid = await bcrypt.compare(password, targetUser.password);
+    if (!isValid)
+      return res.status(401).json({ message: 'Неверный email или пароль' });
+    const user = targetUser.get();
+    delete user.password;
+    const { accessToken, refreshToken } = generateTokens({ user });
+    res
+      .cookie('refreshToken', refreshToken, cookiesConfig)
+      .json({ accessToken, user });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
-//     } catch (error) {
-//         res.status(500).json({ message: error.message });
-//     }
-// })
+router.get('/logout', async (req, res) => {
+  res.clearCookie('refreshToken').json({ message: 'OK' });
+});
 
-// router.get("/logout", async (req, res) => {
-//     res.clearCookie("refreshToken").json({ message: "OK" });
-// })
-
-// router.post("/registr", async (req, res) => {
-//     const { login, password, email } = req.body;
-//     try {
-//       const reg = await Registration.create({ 
-//       login, password, email});
-//       console.log(reg);
-//       res.json(reg);
-//     } catch (error) {
-//       res.status(418).json({ message: error.message });
-//     }
-//   });
+// регистрация рабочая тема
+router.post('/signup', async (req, res) => {
+  const { name, login, password, email } = req.body;
+  try {
+    const reg = await User.create({
+      name,
+      login,
+      password,
+      email,
+    });
+    console.log(reg);
+    res.json(reg);
+  } catch (error) {
+    res.status(418).json({ message: error.message });
+  }
+});
 
 module.exports = router;
-
-
-
